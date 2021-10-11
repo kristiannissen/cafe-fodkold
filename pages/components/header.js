88673e76836357f74d1f2ea10223441e1d2cffbc
kristiannissen <kristian.nissen@gmail.com>
2021-10-11 12:50:26 +0200

    Make it prettier

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,9 +1,11 @@
-
-
 const Header = (props) => {
-  return (<header>
-      <h1 role="heading" aria-level="1">{props.title}</h1>
-    </header>)
-}
+  return (
+    <header>
+      <h1 role="heading" aria-level="1">
+        {props.title}
+      </h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- pages/hello-kitty.js
+++ pages/hello-kitty.js
@@ -1,10 +1,13 @@
 import Header from "./components/header";
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function HelloKitty() {
-    return <div>
+  return (
+    <div>
       <Head>
-      <title>Hello</title>
-    </Head>
-    <Header title="Hello Kitty" /></div>
+        <title>Hello</title>
+      </Head>
+      <Header title="Hello Kitty" />
+    </div>
+  );
 }
