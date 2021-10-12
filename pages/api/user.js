eadc85bc6c0d8c321c2c99f3433a9bb1c7f4de46
kristiannissen <kristian.nissen@gmail.com>
2021-10-12 10:43:24 +0200

    Testing fetch

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,5 +1,3 @@
-
-
 export default function handler(req, res) {
-  res.status(200).json({user: {lat: "hello", lon: "hello"}})
+  res.status(200).json({ user: { lat: "hello", lon: "hello" } });
 }
--- pages/hello-kitty.js
+++ pages/hello-kitty.js
@@ -1,7 +1,7 @@
 import Header from "./components/header";
 import Head from "next/head";
 
-export default function HelloKitty({user}) {
+export default function HelloKitty({ user }) {
   return (
     <div>
       <Head>
