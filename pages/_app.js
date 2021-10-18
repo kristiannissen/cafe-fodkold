7bad8cf3fbac710ec358316d53ac97503bb3fabe
Kristian Nissen <kristian.nissen@gmail.com>
2021-10-18 12:10:59 +0200

    Adding meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Head from "next/head";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
