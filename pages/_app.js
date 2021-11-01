9659882be2080d37faf91ae0fb4f52f0f6f1943f
Kristian Nissen <kristian.nissen@gmail.com>
2021-11-01 13:10:31 +0100

    Service worker added

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ const MyApp = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Component {...pageProps} />
     </>
