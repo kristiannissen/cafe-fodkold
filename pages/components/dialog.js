542671558b00fe494158da0092adce20da359e33
kristiannissen <kristian.nissen@gmail.com>
2021-11-04 14:44:34 +0100

    Styling

--- pages/components/dialog.js
+++ pages/components/dialog.js
@@ -18,24 +18,24 @@ const Dialog = (props) => {
             <div className={styles.dialog}>
               <div className={styles.dialog__container}>
                 <div className={styles.dialog__container__title}>
-                  <span>Dialog Title</span>
-                  <span>
-                    <button onClick={() => props.onHide()}>Close</button>
+                  <span onClick={() => props.onHide()}>
+                    <i className="icon arrow"></i>
                   </span>
+                  <span>Børnenes Kontors pøsevogn</span>
               </div>
                 <div className={styles.dialog__container__content}>
                   <div className={styles.table}>
                     <div className={styles.table__row}>
-                      <div className={styles.table_cell}>Cell</div>
-                      <div className={styles.table_cell}>Cell</div>
+                      <div className={styles.table__cell}>Cell</div>
+                      <div className={styles.table__cell}>Cell</div>
                     </div>
                     <div className={styles.table__row}>
-                      <div className={styles.table_cell}>Cell</div>
-                      <div className={styles.table_cell}>Cell</div>
+                      <div className={styles.table__cell}>Cell</div>
+                      <div className={styles.table__cell}>Cell</div>
                     </div>
                     <div className={styles.table__row}>
-                      <div className={styles.table_cell}>Cell</div>
-                      <div className={styles.table_cell}>Cell</div>
+                      <div className={styles.table__cell}>Cell</div>
+                      <div className={styles.table__cell}>Cell</div>
                     </div>
                   </div>
                 </div>
